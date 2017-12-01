814d4caf02a1998730de946adb2c763e3b5a844b
Nate Yazdani <nyazdani@cs.washington.edu>
2017-12-01 13:33:53 -0800

    Commit

--- src/edu/washington/cse/codestats/runner/SequenceWriter.java
+++ src/edu/washington/cse/codestats/runner/SequenceWriter.java
@@ -23,17 +23,19 @@ public class SequenceWriter {
 	 * HDFS.
 	 */
 	public static void main(final String[] args) throws IOException {
+	    final String directory = new File(args[0]).getAbsolutePath();
 		final Configuration conf = new Configuration();
 		try(final Writer w = SequenceFile.createWriter(conf, Writer.file(new Path(args[1])), Writer.keyClass(Text.class), Writer.valueClass(BytesWritable.class))) {
-            for (String pathname : SequenceWriter.list(args[0])) {
+            for (String pathname : SequenceWriter.list(directory)) {
                 final Text t = new Text();
                 final BytesWritable v = new BytesWritable();
                 final ByteArrayOutputStream baos = new ByteArrayOutputStream();
                 final byte[] classBytes = baos.toByteArray();
                 t.set(pathname.replaceFirst("[.]class", ""));
-                IOUtils.copy(new FileInputStream(new File(args[0] + File.pathSeparator + pathname)), baos);
+                IOUtils.copy(new FileInputStream(new File(directory + File.pathSeparator + pathname)), baos);
                 v.set(classBytes, 0, classBytes.length);
                 w.append(t, v);
+                w.hflush();
             }
 		}
 	}
@@ -45,13 +47,15 @@ public class SequenceWriter {
     }
 
     private static void listUnder(String directory, String subdirectory, List<String> listing)  {
-        for (String filename : new File(directory + File.separator + subdirectory).list()) {
-            String pathname = directory + File.separator + subdirectory + File.pathSeparator + filename;
-            File file = new File(pathname);
+	    System.out.println("Listing directory " + directory + File.pathSeparator + subdirectory + "<- the fuck is this\n");
+	    System.out.println(new File(directory + File.pathSeparator + subdirectory).list());
+        for (String filename : new File(directory + File.pathSeparator + subdirectory).list()) {
+            filename = subdirectory + File.pathSeparator + filename;
+            File file = new File(directory + File.pathSeparator + filename);
             if (file.isDirectory()) {
-                SequenceWriter.listUnder(directory, subdirectory + File.pathSeparator + filename, listing);
-            } else if (file.isFile() && pathname.endsWith(".class")) {
-                listing.add(subdirectory + File.pathSeparator + filename);
+                SequenceWriter.listUnder(directory, filename, listing );
+            } else if (file.isFile() && filename.endsWith(".class")) {
+                listing.add(filename);
             }
         }
     }
